e838b012ef2433cb7aa504a993ed55f732c7bb16
jambrizz <ambrizjovani@gmail.com>
2024-10-22 21:55:31 -0700

    Creation of new branch and the creation of the seller's portal

---
+++ pages/sellerPortal.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+
+const SellerPortal = () => {
+
+
+    return (
+        <>
+            <h1>Seller portal</h1>
+        </>
+    );
+}
+
+export default SellerPortal
